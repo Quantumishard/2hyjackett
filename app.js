e001f23f5bf98c6812916e80bc5edbb8972564b2
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2023-08-14 13:57:58 +0100

    Update app.js

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ const fetchTorrent = async (query) => {
           accept: "*/*",
           "accept-language": "en-US,en;q=0.9",
           "x-requested-with": "XMLHttpRequest",
-          cookie: "Jackett=YOUR_COOKIE_HERE", // Replace with your host cookie
+          cookie: "Jackett=CfDJ8AG_XUDhxS5AsRKz0FldsDJIHUJANrfynyi54VzmYuhr5Ha5Uaww2hSQytMR8fFWjPvDH2lKCzaQhRYI9RuK613PZxJWz2tgHqg1wUAcPTMfi8b_8rm1Igw1-sZB_MnimHHK7ZSP7HfkWicMDaJ4bFGZwUf0xJOwcgjrwcUcFzzsVSTALt97-ibhc7PUn97v5AICX2_jsd6khO8TZosaPFt0cXNgNofimAkr5l6yMUjShg7R3TpVtJ1KxD8_0_OyBjR1mwtcxofJam2aZeFqVRxluD5hnzdyxOWrMRLSGzMPMKiaPXNCsxWy_yQhZhE66U_bVFadrsEeQqqaWb3LIFA", // Replace with your host cookie
         },
         referrerPolicy: "no-referrer",
         method: "GET",
