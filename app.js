f576dc52313eaf974c58597819e4c2ce58c4da29
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2023-08-15 20:48:55 +0100

    Update app.js

--- app.js
+++ app.js
@@ -187,7 +187,7 @@ const host2 = {
 
 const fetchTorrentFromHost1 = async (query) => {
   const { hostUrl, apiKey } = host1;
-  const url = `${hostUrl}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&Category%5B%5D=2000&Category%5B%5D=5000&Tracker%5B%5D=limetorrents`;
+  const url = `${hostUrl}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&Category%5B%5D=2000&Category%5B%5D=5000&Tracker%5B%5D=torrent9`;
 
   try {
     const response = await fetch(url, {
