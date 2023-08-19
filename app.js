5dac6d673efd15b9630ea3d09a968dd58a538088
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2023-08-19 21:38:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -248,9 +248,9 @@ const fetchTorrentFromHost1 = async (query) => {
   }
 };
 
-//const fetchTorrentFromHost2 = async (query) => {
+  const fetchTorrentFromHost2 = async (query) => {
   const { hostUrl, apiKey } = host2;
-  const url = `${hostUrl}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&Category[]=2000&Category[]=2040&Category[]=2045&Category[]=2080&Category[]=5000&Category[]=5040&Category[]=5045&Category[]=5080&Tracker%5B%5D=torrentfunk`;
+  const url = `${hostUrl}/api/v2.0/indexers/all/results?apikey=${apiKey}&Query=${query}&Category[]=2000&Category[]=2040&Category[]=2045&Category[]=2080&Category[]=5000&Category[]=5040&Category[]=5045&Category[]=5080&Tracker%5B%5D=torrent9`;
 
   try {
     const response = await fetch(url, {
