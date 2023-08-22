22ae0019e31d062ad8761ef100383cf8553be3d1
mikmc55 <70898105+mikmc55@users.noreply.github.com>
2023-08-22 18:24:14 +0100

    Update app.js

--- app.js
+++ app.js
@@ -105,8 +105,6 @@ if (!parsed.files && uri.startsWith("magnet")) {
   };
 };
 
-const http = require("http");
-
 const isRedirect = async (url) => {
   return new Promise((resolve, reject) => {
     const timeoutId = setTimeout(() => {
